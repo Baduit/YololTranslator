b39a45d1740a3541bca2d41b0283e1efae8cdac9
Simon Bertho <simon.bertho@epitech.eu>
2019-01-21 12:50:51 +0100

    the bot should not crash anymore

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -3,6 +3,9 @@ const exec = require('child_process').exec;
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+let nbError = 0;
+let nbException = 0;
+
 function extract_usefull_content(message_content, command_name) {
 	let i = command_name.length;
 	while (i < message_content.length) {
@@ -13,18 +16,41 @@ function extract_usefull_content(message_content, command_name) {
 	return message_content.substr(i, message_content.length)
 }
 
-bot.on('message', function (message) {
-	const cmd_name = "!yolol"
-    if (message.content.search(cmd_name) == 0) {
-		let sentence = extract_usefull_content(message.content, cmd_name)
-		if (!(sentence === "")) {
-			exec('../YololTranslator.exe "' + sentence + '" ../dico/', (error, stdout, stderr) => {
-				message.channel.send(stdout)
-			});
-		} else {
-			message.channel.send('Please, do not send empty an empty sentence, it is even more useless than this bot.')
+bot.on('message', (message) => {
+	try {
+		const cmd_name = "!yolol"
+		if (message.content.search(cmd_name) == 0) {
+			let sentence = extract_usefull_content(message.content, cmd_name)
+			if (!(sentence === "")) {
+				exec('../YololTranslator.exe "' + sentence + '" ../dico/', (error, stdout, stderr) => {
+					message.channel.send(stdout)
+				});
+			} else {
+				message.channel.send('Please, do not send empty an empty sentence, it is even more useless than this bot.')
+			}
 		}
-    }
+	} catch (exception) {
+		nbException++;
+		console.log(exception);
+	}
+})
+
+bot.on('message', (message) => {
+	try {
+		const cmd_name = "!log-yolol"
+		if (message.content.search(cmd_name) == 0) {
+			message.channel.send('Errors: ' + nbError + '\nExceptions: ' + nbException)
+		}
+	} catch (exception) {
+		nbException++;
+		console.log(exception);
+	}
+})
+
+
+bot.on('error', (error) => {
+	nbError++;
+	console.log(error)
 })
 
 var fs = require('fs');
