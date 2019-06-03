b4c9e12c8477f0a40299248721bca4e2ba720183
Lukas Parant <lparant@devatics.com>
2019-06-03 14:03:05 +0200

    npm watch config

--- web/index.js
+++ web/index.js
@@ -1,25 +1,28 @@
 const port = 4910;
 
-var exec = require('child_process').exec;
+var exec = require("child_process").exec;
 
-var app = require('express')();
+var app = require("express")();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route('/').get(function(req, res) { 
-    res.status(200).send('Welcome on Yolol Translator API! Send a POST request on /translate with the text you want to translate in a parameter named "text".'); 
+app.route("/").get(function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.route('/translate').post(function(req, res) {
-	if (!req.body.text)
-		return res.status(400).send('You need to specify the "text" parameter.');
-	exec('../YololTranslator.exe "' + req.body.text + '" ../dico/', (error, stdout, stderr) => {
-		res.status(200).send(stdout);
-	});
-}); 
+app.route("/translate").post(function(req, res) {
+  if (!req.body.text)
+    return res.status(400).send('You need to specify the "text" parameter.');
+  exec(
+    '../YololTranslator.exe "' + req.body.text + '" ../dico/',
+    (error, stdout, stderr) => {
+      res.status(200).send(stdout);
+    }
+  );
+});
 
 app.listen(port);
 
