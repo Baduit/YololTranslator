3dd2eb98b18f5819715fe8a6eccfb7c80f783202
Simon Bertho <simon.bertho@epitech.eu>
2019-08-04 12:20:43 +0200

    Site V2

---
+++ core/v2/src/SharedLibInterface.cpp
@@ -0,0 +1,108 @@
+#include <cstring>
+#include <cctype>
+
+#include "SharedLibInterface.hpp"
+#include "Split.hpp"
+
+namespace
+{
+
+std::string to_lower(std::string_view str)
+{
+	std::string result;
+	result.reserve(str.size());
+
+	for (auto c: str)
+		result += static_cast<char>(tolower(c));
+
+	return result;
+}
+
+}
+
+YololResult init_translator(std::string_view phonems_list_filename, std::string_view words_phonem_filename, std::string_view word_dict_filename)
+{
+	try
+	{
+		translator.emplace(phonems_list_filename, words_phonem_filename, word_dict_filename);
+		return YOLOL_SUCCESS;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception in init_translator: " << e.what() << std::endl;
+		return YOLOL_FAILURE;
+	}
+}
+
+char* translate_translator(std::string_view input)
+{
+	try
+	{
+		auto lower_case_input = to_lower(input);
+		std::string output;
+		auto words = tokenize(lower_case_input, {" ", ";", ",", ".", "\t", "\n", "!", "?", ":"});
+		for (const auto w: words)
+		{
+			if (w.type == Token::Type::WORD)
+			{
+				try
+				{
+					output += translator->translate(w.value);
+				}
+				catch (std::runtime_error& e) // to handle unknown words, i should make designated exception or handle it in the translator or better use optional
+				{
+					output += w.value;
+				}
+			}
+			else // if is token
+			{
+				if (w.value == "?" || w.value == "!")
+				{
+					output += w.value;
+					output += w.value;
+				}
+				else if (w.value == "\n")
+				{
+					output += w.value;
+				}
+				else
+				{
+					output += " ";
+				}
+			}
+		}
+		output += "\n";
+
+		return strdup(output.c_str());
+	}
+	catch (std::exception& e)
+	{
+		return strdup(e.what());
+	}
+}
+
+extern "C"
+{
+
+int	init(const char* phonems_list_filename, const char* words_phonem_filename, const char* word_dict_filename)
+{
+	return init_translator(phonems_list_filename, words_phonem_filename, word_dict_filename);
+}
+
+// do not forget to delete output ptr
+char*	translate(const char* input)
+{
+	return translate_translator(input);
+}
+
+void				delete_string(char *str)
+{
+	free(str);
+}
+
+void			destroy()
+{
+	translator.reset();
+}
+
+}
--- web/index.js
+++ web/index.js
@@ -1,24 +1,51 @@
+// Constants
 const port = 4910;
-
-var exec = require("child_process").execFile;
-
-var express = require("express");
-var app = express();
-
-const bodyParser = require("body-parser");
-
-app.use(express.static("public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.route("/").get(function(req, res) {
-	res.sendFile(__dirname + "/public/index.html");
-});
 
 const DICO_V1_PATH = '../core/v1/dico/'
 const V2_WORD_TO_WORD = '../core/v2/assets/word_to_word_dict_fr.json'
 const V2_WORD_TO_PHONEMS = '../core/v2/assets/fr.dict'
 const V2_PHONEMS_TO_CHARS = '../core/v2/assets/french_dico.json'
+
+// Depedencies
+var exec = require("child_process").execFile;
+
+const bodyParser = require("body-parser");
+var express = require("express");
+var app = express();
+
+const ref = require('ref');
+var ffi = require('ffi');
+
+// LibYololHandling
+var libYolol = ffi.Library('../core/v2/libYololTranslator', {
+	'init': [ 'int', [ 'string', 'string', 'string' ] ],
+	'translate': [ 'void*', [ 'string' ] ],
+	'delete_string': [ 'void', [ 'void*' ] ],
+	'destroy': [ 'void', [] ]
+});
+if (libYolol.init(V2_PHONEMS_TO_CHARS, V2_WORD_TO_PHONEMS, V2_WORD_TO_WORD) == 0)
+{
+	throw Error("Error while initializing libYolol")
+}
+
+// Proper cleanup
+process.on('exit', function () {
+	libYolol.destroy()
+});
+
+
+
+// App init
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Routes
+app.route("/").get(function(req, res) {
+	res.sendFile(__dirname + "/public/index.html");
+});
+
 
 app.route("/translate").post(function(req, res)
 {
@@ -44,8 +71,16 @@ app.route("/translate").post(function(req, res)
 			}
 		);
 	} else if (req.body.version == "v2") {
-		// exec args will change
-		res.status(200).send("Not implemented yet");
+		var translated_sentence = libYolol.translate(req.body.text);
+		if (translated_sentence == null)
+		{
+			res.status(200).send("Invalid input");
+		}
+		else
+		{
+			res.status(200).send(ref.readCString(translated_sentence, 0));
+			libYolol.delete_string(translated_sentence); // maybe better in a then of send
+		}
 	}
 });
 
@@ -53,6 +88,7 @@ app.route("/test").post(function(req, res) {
 	res.status(200).send(req.body.text);
 });
 
+// Start
 app.listen(port);
 
 console.log("Server is running on port: " + port);
