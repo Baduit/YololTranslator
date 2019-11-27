9765f8f11d1a5e08adea31261d37a9487883c989
Baduit <simon.bertho@epitech.eu>
2019-11-27 22:24:14 +0100

    Little refactoring and implementing vdd

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -9,13 +9,12 @@ const V2_WORD_TO_PHONEMS = '../core/v2/assets/fr.dict'
 const V2_PHONEMS_TO_CHARS = '../core/v2/assets/french_dico.json'
 
 var libYolol = ffi.Library('../core/v2/libYololTranslator', {
-	'init': [ 'int', [ 'string', 'string', 'string' ] ],
-	'translate': [ 'void*', [ 'string' ] ],
-	'delete_string': [ 'void', [ 'void*' ] ],
-	'destroy': [ 'void', [] ]
+	'init': ['int', ['string', 'string', 'string']],
+	'translate': ['void*', ['string']],
+	'delete_string': ['void', ['void*']],
+	'destroy': ['void', []]
 });
-if (libYolol.init(V2_PHONEMS_TO_CHARS, V2_WORD_TO_PHONEMS, V2_WORD_TO_WORD) == 0)
-{
+if (libYolol.init(V2_PHONEMS_TO_CHARS, V2_WORD_TO_PHONEMS, V2_WORD_TO_WORD) == 0) {
 	throw Error("Error while initializing libYolol")
 }
 
@@ -44,10 +43,9 @@ const EMPTY_MESSAGE_ANSWER = 'Please, do not send empty an empty sentence, it is
 const DICO_V1_PATH = '../core/v1/dico/'
 
 
-function exec_v1(message, sentence)
-{
+function exec_v1(message, sentence) {
 	if (!(sentence === "")) {
-		exec('../core/v1/YololTranslator.exe', [ sentence, DICO_V1_PATH], (error, stdout, stderr) => {
+		exec('../core/v1/YololTranslator.exe', [sentence, DICO_V1_PATH], (error, stdout, stderr) => {
 			message.channel.send(stdout)
 		});
 	} else {
@@ -55,38 +53,56 @@ function exec_v1(message, sentence)
 	}
 }
 
+function translate_v2(message, sentence) {
+	var translated_sentence = libYolol.translate(sentence);
+	if (translated_sentence == null) {
+		message.channel.send("Invalid input");
+	} else {
+		message.channel.send(ref.readCString(translated_sentence, 0));
+		libYolol.delete_string(translated_sentence);
+	}
+}
+
+async function translate_vdd(message) {
+	try {
+
+		let message_map = await message.channel.fetchMessages({ limit: 10 });
+		let messages = message_map.array();
+		let my_message_found = false;
+		for (let i = 0; i < messages.length; ++i) {
+			if (my_message_found) {
+				translate_v2(message, messages[i].content);
+				break;
+			} else if (messages[i].id == message.id) {
+				my_message_found = true;
+			}
+		}
+	} catch (e) {
+		++nbException;
+	}
+}
+
 bot.on('message', (message) => {
 	try {
+		if (message.author.bot) {
+			return;
+		}
+
 		const cmd_name = "!yolol "
 		const v1_cmd_name = "!yolol-v1 "
 		const v2_cmd_name = "!yolol-v2 "
+		const vdd_cmd_name = "!vdd"
 		if (message.content.search(cmd_name) == 0) {
 			let sentence = extract_usefull_content(message.content, cmd_name)
-			var translated_sentence = libYolol.translate(sentence);
-			if (translated_sentence == null)
-			{
-				message.channel.send("Invalid input");
-			}
-			else
-			{
-				message.channel.send(ref.readCString(translated_sentence, 0));
-				libYolol.delete_string(translated_sentence);
-			}
+			translate_v2(message, sentence)
 		} else if (message.content.search(v1_cmd_name) == 0) {
 			let sentence = extract_usefull_content(message.content, v1_cmd_name)
 			exec_v1(message, sentence)
 		} else if (message.content.search(v2_cmd_name) == 0) {
 			let sentence = extract_usefull_content(message.content, v2_cmd_name)
-			var translated_sentence = libYolol.translate(sentence);
-			if (translated_sentence == null)
-			{
-				message.channel.send("Invalid input");
-			}
-			else
-			{
-				message.channel.send(ref.readCString(translated_sentence, 0));
-				libYolol.delete_string(translated_sentence);
-			}
+			translate_v2(message, sentence)
+		} else if (message.content.search(vdd_cmd_name) == 0) {
+			translate_vdd(message);
 		}
 	} catch (exception) {
 		nbException++;
@@ -113,8 +129,8 @@ bot.on('error', (error) => {
 })
 
 var fs = require('fs');
- 
-fs.readFile(require('os').homedir() + '/bot_tokens/yolol', 'utf8', function(err, contents) {
+
+fs.readFile(require('os').homedir() + '/bot_tokens/yolol', 'utf8', function (err, contents) {
 	if (err) {
 		console.log(err)
 	} else {
