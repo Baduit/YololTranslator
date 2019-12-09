d4e9959c0f8aab1da4d363cc5ba682e3568c003a
Baduit <simon.bertho@epitech.eu>
2019-12-09 22:18:42 +0100

    Update page title

--- gui_client/FlutterApp/yolol_translator/lib/main.dart
+++ gui_client/FlutterApp/yolol_translator/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Yolol Translator Demo Home Page'),
+      home: MyHomePage(title: 'Yolol Translator'),
     );
   }
 }
