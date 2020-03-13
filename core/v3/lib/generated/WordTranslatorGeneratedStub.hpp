0348a029438bb599e4e397d75c18d61ef3e6c4d7
Simon Bertho <simon.bertho@epitech.eu>
2020-03-13 20:35:17 +0100

    Fill a bit the stub

--- core/v3/lib/generated/WordTranslatorGeneratedStub.hpp
+++ core/v3/lib/generated/WordTranslatorGeneratedStub.hpp
@@ -20,33 +20,33 @@ StaticMap<TranslatorCallable, WORD_TRANSLATOR_SIZE> load_word_translator_map()
 		{
 			// Keys
 			{
-				"",
-				"",
-				"",
-				"",
-				""
+				"test",
+				"salut",
+				"bonjour",
+				"hey",
+				"rien"
 			},
 			// Values
 			{
 				WordTranslationContainer<1>
 					{
-						{ WordTranslation{ 1, "" } }
+						{ WordTranslation{ 1, "taist" } }
 					},
 				WordTranslationContainer<2>
 					{
-						{ WordTranslation{ 1, "" }, WordTranslation{ 1, "" } }
+						{ WordTranslation{ 1, "slt" }, WordTranslation{ 1, "salu" } }
 					},
 				WordTranslationContainer<2>
 					{
-						{ WordTranslation{ 1, "" }, WordTranslation{ 1, "" } }
+						{ WordTranslation{ 1, "bjr" }, WordTranslation{ 1, "bonjoure" } }
 					},
 				WordTranslationContainer<3>
 					{
-						{ WordTranslation{ 1, "" }, WordTranslation{ 1, "" }, WordTranslation{ 1, "" } }
+						{ WordTranslation{ 1, "heyy" }, WordTranslation{ 1, "heyyy" }, WordTranslation{ 1, "heyyyy" } }
 					},
 				WordTranslationContainer<4>
 					{
-						{ WordTranslation{ 1, "" }, WordTranslation{ 1, "" }, WordTranslation{ 1, "" }, WordTranslation{ 1, "" } }
+						{ WordTranslation{ 1, "ri1" }, WordTranslation{ 1, "ryen" }, WordTranslation{ 1, "ry1" }, WordTranslation{ 1, "rill1" } }
 					}
 			}
 		};
