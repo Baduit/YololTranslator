0fef3f824ed9f842290960a55c15205a5f8e1856
Baduit <simon.bertho@epitech.eu>
2020-04-12 00:39:38 +0200

    Update constante

--- core/v3/lib/PhonemTranslation.hpp
+++ core/v3/lib/PhonemTranslation.hpp
@@ -18,7 +18,7 @@ struct PhonemUniqueTranslation
 
 struct PhonemCompositionTranslation
 {
-	std::array<Phonem, 3> phonem; // TODO: shrink to fit later with generated constant
+	std::array<Phonem, 5> phonem; // TODO: shrink to fit later with generated constant
 	std::size_t nb_phonem; // Better than use an array of optional I think
 	std::array<PhonemEquivalent, 10> _equivalents; // TODO: shrink to fit later with generated constant
 };
