bf7cac7ceae386e5fb6ad8ee3dc074c25c045d84
Baduit <simon.bertho@epitech.eu>
2020-06-15 00:53:56 +0200

    Add constant

--- core/v3/lib/PhonemList.hpp
+++ core/v3/lib/PhonemList.hpp
@@ -8,11 +8,13 @@
 namespace YololTranslator
 {
 
+constexpr std::size_t PHONEM_LIST_MAX_SIZE = 50;
+
 // Nothing should have more than 50 phonems, later I can put it in a constant that will be set in the generated file to fit perfectly
 // later add more template et type erase if startup time is to slow (but it would add a virtual call during the translation)
 struct PhonemList
 {
-	std::array<Phonem, 50> phonems; 
+	std::array<Phonem, PHONEM_LIST_MAX_SIZE> phonems; 
 	std::size_t size;
 };
 
