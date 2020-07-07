18e8b9e7ce7b6d90eca432f6f9de4e78352852b6
Baduit <simon.bertho@epitech.eu>
2020-07-07 20:59:10 +0200

    Add colored logs so it seems cool even if it is not

--- core/v3/code_generator/generate.py
+++ core/v3/code_generator/generate.py
@@ -2,8 +2,23 @@ import argparse
 import json
 from termcolor import colored, cprint
 
-""" step_colored_text = colored(current_step + ':', 'blue', attrs=['bold'])
-text_colored = colored(text, 'green') """
+INFO = "Info"
+WARNING = "Warning"
+ERROR = "Error"
+
+def log(log_type, step, message):
+	if log_type == INFO:
+		colored_log_type = colored(log_type, 'green', attrs=['bold'])
+	elif log_type == WARNING:
+		colored_log_type = colored(log_type, 'yellow', attrs=['bold'])
+	elif log_type == ERROR:
+		colored_log_type = colored(log_type, 'red', attrs=['bold'])
+	else:
+		colored_log_type = colored(log_type, 'blue', attrs=['bold'])
+	
+	colored_step = colored(step, 'blue')
+	print(colored_log_type + ': ' + colored_step + ' => ' + message)
+
 
 def read_json_file(filename: str):
 	with open(filename, "r") as file:
@@ -14,15 +29,20 @@ def read_lines_file(filename: str):
 		return file.readlines(106000)
 
 def generate_word_to_word(json_content):
-	pass
+	log(INFO, "Word to word", "Generation starting")
+	log(INFO, "Word to word", "Generation done")
 
 def generate_word_to_phonem(lines_file):
-	pass
+	log(INFO, "Word to phonem", "Generation starting")
+	log(INFO, "Word to phonem", "Generation done")
 
 def generate_phonem_to_chars(json_content):
-	pass
+	log(INFO, "Phonem to chars", "Generation starting")
+	log(INFO, "Phonem to chars", "Generation done")
 
 def main():
+	log(INFO, "Main", "Program is starting.")
+
 	parser = argparse.ArgumentParser(description='Generate dictionnaries for YololTranslator Core V3')
 	parser.add_argument('-wwf', '--word-to-word-file', default="./assets/word_to_word_dict_fr.json", help='Path to the input file for word to word translation.')
 	parser.add_argument('-wpf', '--word-to-phonem-file', default="./assets/fr.dict", help='Path to the input file for word to phonem translation.')
@@ -30,9 +50,13 @@ def main():
 	parser.add_argument('-o', '--output-dir', default="./lib/generated", help='Path to the output directory.')
 	args = parser.parse_args()
 
+	log(INFO, "Main", "Arguments parsing done.")
+
 	generate_phonem_to_chars(read_json_file(args.word_to_word_file))
 	generate_word_to_phonem(read_lines_file(args.word_to_phonem_file))
 	generate_word_to_word(read_json_file(args.phonem_to_char_file))
+
+	log(INFO, "Main", "Program is over.")
 
 if __name__ == "__main__":
 	main()
