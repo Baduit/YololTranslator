d7db1cd4f84bbc9ebb5c6d432493d9c57e9afb2d
Baduit <simon.bertho@epitech.eu>
2020-08-03 20:17:19 +0200

    Add ut

--- core/v2/test/Split/main.cpp
+++ core/v2/test/Split/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include <cassert>
+#include <vector>
+#include <string>
+#include <string_view>
+
+#include <boost_ut/ut.hpp>
+
+#include <utilities/Split.hpp>
+
+using namespace boost::ut;
+
+void split_simple()
+{
+	std::vector<std::string_view> expected_result { "Salut", "les", "gens" };
+
+	auto strings = split("Salut les gens", {" "});
+	expect(strings.size() == expected_result.size()) << "Check the size";
+	expect(strings == expected_result) << "Check the elements";
+}
 
 int main()
 {
-	std::cout << "Hello, World !" << std::endl;
-	assert(true);
+	"case"_test = split_simple;
 }
