a717f3bb8048d357f7d5c42c0ce11f3ba7bf78e5
Baduit <lena.bertho@protonmail.com>
2021-06-20 22:18:23 +0200

    Remove deprecated v1 Remove the deprecated c API Begin the new discord bot Move some files around

--- discord-bot/bash_scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-nodejs index.js &
-echo $! > pid.txt
--- discord-bot/bash_scripts/ping.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-if ps -p `cat pid.txt` >> /dev/null ; then echo "Bot is running"; else echo "Bot is not running"; fi
