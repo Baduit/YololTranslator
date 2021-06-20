af11339822c6c9924f3881871c240c5fe039acea
Baduit <lena.bertho@protonmail.com>
2021-06-21 00:44:34 +0200

    discord cpp integrated finally

--- discord-bot/src/main.cpp
+++ discord-bot/src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <dpp.h>
 
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	const std::string token = "BOT_TOKEN_HERE";
+	dpp::Client client;
+
+	client.onReady =
+		[&]()
+		{
+			std::cout << "hello, c++!\n";
+			std::cout << client.user.id << "\n\n";
+		};
+
+	client.onMessage =
+		[&](const dpp::Message &message)
+		{
+			if (message.content == "jaaj?")
+			{
+				message.channel().send("jooj"); // or message.reply("jooj"), which mentions the message author
+			}
+		};
+
+	client.run(token);
 }
