1d7943abe36cab69093f42e789de0f4e336b641b
Baduit <lena.bertho@protonmail.com>
2021-10-22 01:15:37 +0200

    Setup discord bot

--- discord_bot/index.js
+++ discord_bot/index.js
@@ -1,1 +1,8 @@
+import WebSocket from 'ws';
+import * as YololSDK from '../nodejs_sdk/YololSDK.mjs'
+
+let client = new YololSDK.Client(WebSocket, "ws://localhost:4577");
+
+// todo discord stuff
+
 console.log("hey")
