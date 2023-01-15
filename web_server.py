c19a9945cda3985342f99cea097eadcf9f339368
Lena Bertho <lena.bertho@protonmail.com>
2023-01-15 20:22:35 +0100

    Omg, i'm such a moron

--- web_server.py
+++ web_server.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
-from core.build.Debug.yolol import Translator
+from core.build.yolol import Translator
 
 
 yolol = Translator(word_to_phonem_file='core/assets/french_dico.json', phonem_to_chars_file='core/assets/fr.dict', word_to_word_file='core/assets/word_to_word_dict_fr.json')
